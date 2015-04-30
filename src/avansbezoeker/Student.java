d981cfbe4a48ef80d1c5650088306b69caecdb16
Dominique Rietveld <rietvelddominique@gmail.com>
2015-04-30 11:48:36 +0200

    Student done

--- src/avansbezoeker/Student.java
+++ src/avansbezoeker/Student.java
@@ -1,9 +1,9 @@
 package avansbezoeker;
 
 public class Student extends AvansVisitor implements Gamer{
-
+    
     @Override
     public String getGame() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "Wow";
     }
 }

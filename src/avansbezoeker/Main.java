5db6b2d3eb0cd3174736b115490c1ae52bb273d1
Dominique Rietveld <rietvelddominique@gmail.com>
2015-05-11 22:53:53 +0200

    final

--- src/avansbezoeker/Main.java
+++ src/avansbezoeker/Main.java
@@ -10,12 +10,17 @@ package avansbezoeker;
  * @author Dominique Rietveld
  */
 public class Main {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Teacher teacher1 = new Teacher("Henk de Vries","DB les geven","PVO voorzitten");
+        Teacher teacher2 = new Teacher("Arie Sjofel","EC les geven","");
+        AvansVisitor student1 = new Student("Sjaak Bromsnor","DB les volgen");
+        teacher1.print();
+        teacher2.print();
+        student1.print();
     }
 
 }
